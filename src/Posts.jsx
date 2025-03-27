4f4e6e618bf4f7a4b66f8ddb8eb583d46e1a18dd
Eero Timonen <eero.timonen@student.careeria.fi>
2025-03-27 11:05:32 +0200

    posts päivitys

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,6 +5,7 @@ const Posts = () => {
 
 // Komponentin tilan määritys
 const [posts, setPosts] = useState([])
+const [showPosts, setShowPosts] = useState(false)
 
 useEffect(() => {
   fetch("https://jsonplaceholder.typicode.com/posts")
@@ -15,15 +16,15 @@ useEffect(() => {
 
   return (
       <>
-        <h2>Posts from typicode</h2>
+        <h2 onClick={() => setShowPosts(!showPosts)}>Posts from typicode</h2>
 
         {
-          posts && posts.map(p => 
+          showPosts && posts && posts.map(p => 
             <div className='posts' key={p.id}>
-            <br/>
-            <h3>{p.title}</h3>
-            <p>{p.body}</p>
-            <br/>
+              <br/>
+              <h3>{p.title} (User: {p.userId})</h3>
+              <p>{p.body}</p>
+              <br/>
             </div>
           )
         }
